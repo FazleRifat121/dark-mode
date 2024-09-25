eeab70bd68a6e8c29b2c690f8312099344ba64f2
FazleRifat <fazlerifatofficial@gmail.com>
2024-09-25 17:55:27 +0600

    add some effect

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-const day = document.querySelector('.day');
+const day = document.querySelector(".day");
 
 const night = document.querySelector(".night");
 
-day.addEventListener("click", function(){
-    document.body.style.backgroundColor = "white";
-    document.body.style.color = "black";
+day.addEventListener("click", function () {
+  document.body.style.backgroundColor = "white";
+  document.body.style.color = "black";
+  night.style.visibility = "visible";
+  day.style.visibility = "hidden";
 });
-night.addEventListener("click", function(){
-    document.body.style.backgroundColor = "black";
-    document.body.style.color = "white";
+night.addEventListener("click", function () {
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "white";
+  night.style.visibility = "hidden";
+  day.style.visibility = "visible";
 });
